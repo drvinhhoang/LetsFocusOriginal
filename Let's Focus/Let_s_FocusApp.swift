44f32744dc304264499438ee92803b29549da4b3
vinh <dr.vinhhoang@gmail.com>
2021-10-12 08:58:48 +0700

    Initial Commit

---
+++ Let's Focus/Let_s_FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Let_s_FocusApp.swift
+//  Let's Focus
+//
+//  Created by VINH HOANG on 12/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Let_s_FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Let's FocusTests/Let_s_FocusTests.swift
@@ -0,0 +1,33 @@
+//
+//  Let_s_FocusTests.swift
+//  Let's FocusTests
+//
+//  Created by VINH HOANG on 12/10/2021.
+//
+
+import XCTest
+@testable import Let_s_Focus
+
+class Let_s_FocusTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
